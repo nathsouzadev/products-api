561e015879dca8c4e72c86c3cf8cbafd33150177
Nathally Souza <72207361+nathyts@users.noreply.github.com>
2022-11-26 07:10:39 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,5 +5,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   await app.listen(process.env.PORT || 3000);
+  console.log('Service on')
 }
 bootstrap();
